f33660b3c522eb25ee2b10d8dc2a2d772db4fd9f
MarcoGranata9 <granatam9900@gmail.com>
2024-05-21 16:11:45 +0200

    first commit

--- src/org/learning/Book.java
+++ src/org/learning/Book.java
@@ -1,2 +1,73 @@
-package org.learning;public class Book {
+package org.learning;
+
+public class Book {
+    // Attributi
+    private String title;
+    private int pageNum;
+    private String author;
+    private String publisher;
+
+    // Costruttore
+    public Book(String title, int pageNum, String author, String publisher) throws IllegalArgumentException {
+        checkString(title, "Il titolo non può essere vuoto");
+        checkString(author, "L'autore non può essere vuoto");
+        checkString(publisher, "L'editore non può essere vuoto");
+        checkNumber(pageNum);
+
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.pageNum = pageNum;
+    }
+    // Metodi
+
+    private void checkString(String str, String msg) throws IllegalArgumentException{
+        if (str == null || str.isEmpty()) {
+            throw new IllegalArgumentException(msg);
+        }
+    }
+
+    private void checkNumber(int num) throws IllegalArgumentException{
+        if (num <= 0) {
+            throw new IllegalArgumentException("Il numero non può essere inferiore o uguale a 0");
+        }
+    }
+
+
+    // Setter / Getter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) throws IllegalArgumentException{
+        checkString(title, "Il titolo non può essere vuoto");
+        this.title = title;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) throws IllegalArgumentException{
+        checkNumber(pageNum);
+        this.pageNum = pageNum;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) throws IllegalArgumentException{
+        checkString(author, "L'autore non può essere vuoto");
+        this.author = author;
+    }
+
+    public String getEditor() {
+        return publisher;
+    }
+
+    public void setEditor(String publisher) throws IllegalArgumentException{
+        checkString(publisher, "L'editore non può essere vuoto");
+        this.publisher = publisher;
+    }
 }
--- src/org/learning/Main.java
+++ src/org/learning/Main.java
@@ -1,2 +1,84 @@
-package org.learning;public class Main {
+package org.learning;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner
+        Scanner scanner = new Scanner(System.in);
+        // Chiedo all'utente quanti libri vuole inserire inizializzo array
+        System.out.println("Quanti libri vuoi aggiungere?");
+        int arrayL = 0;
+        try {
+            arrayL = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("ERRORE: Hai inserito un numero/carattere non valido");
+        }
+        Book[] books = new Book[arrayL];
+
+        // Itero array facendo inserire i dati dei libri all' utente
+        for (int i = 0; i < books.length; i++) {
+            System.out.println("Inserisci il nome del libro");
+            String name = scanner.nextLine();
+
+            System.out.println("Inserisci il numero delle pagine");
+            int pages = 0;
+            try {
+                pages = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("ERRORE: Hai inserito un carattere non valido");
+            }
+
+            System.out.println("Inserisci l'autore");
+            String author = scanner.nextLine();
+
+            System.out.println("Inserisci l'editore");
+            String publisher = scanner.nextLine();
+
+            try {
+                books[i] = new Book(name, pages, author, publisher);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+        // chiudo scanner
+        scanner.close();
+
+        // Prendo il file books.txt
+        File booksTxt = new File("./resources/books.txt");
+        System.out.println(booksTxt.exists());
+        // try-with-resources per scrivere il file
+        try (FileWriter fileWriter = new FileWriter(booksTxt)) {
+            for (int i = 0; i < books.length; i++) {
+                fileWriter.write("Titolo: " + books[i].getTitle() +"\n");
+                fileWriter.write("Pagine: " + books[i].getPageNum()+"\n");
+                fileWriter.write("Autore: " + books[i].getAuthor()+"\n");
+                fileWriter.write("Editore: " + books[i].getEditor()+"\n");
+                fileWriter.write("-----------------------------------------\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Impossibile scrivere sul file");
+        }
+
+        // Leggo file
+        Scanner fileReader = null;
+        try {
+            fileReader = new Scanner(booksTxt);
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Impossibile leggere il file");
+        } finally {
+            if (fileReader != null) {
+                fileReader.close();
+            }
+        }
+    }
 }
